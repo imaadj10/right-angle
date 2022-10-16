42f62e89cea2b8597372181623ab47a4862b593d
Bob Pham <bobbaopham@gmail.com>
2022-10-16 00:50:40 -0700

    centered

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,9 @@ function App() {
 
   return <div className="bg-gradient-to-tr from-indigo-300 to-purple-400 animate-gradient-xy grid place-items-center h-screen">
     <Title />
-    <Cam />
+    <div className="w-1/2 h-auto grid place-items-center">
+      <Cam />
+    </div>
     <button onClick={refreshPage} className="inline-block px-6 py-2.5  bg-blue-600 text-white font-medium text-xl leading-tight uppercase rounded-full shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
     Calibrate
     </button>
