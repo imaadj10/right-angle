876da96edf9e6a693576376e7f3f27074a4ecc19
Imaad Junaidi <imaadjunaidi1011@gmail.com>
2022-10-15 22:19:03 -0700

    Added mediapipe pose to browser camera

--- server/posture_corrector/routes.py
+++ server/posture_corrector/routes.py
@@ -1,19 +1,47 @@
 from posture_corrector import app
 from flask import render_template, Response
-from posture_corrector.camera import VideoCamera
+import mediapipe as mp
+import cv2
+import time
 
-@app.route('/')
+def generate_frames():
+    previous_time = 0
+    # creating our model to draw landmarks
+    mpDraw = mp.solutions.drawing_utils
+    # creating our model to detected our pose
+    my_pose = mp.solutions.pose
+    pose = my_pose.Pose()
+
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        success, img = cap.read()
+        # converting image to RGB from BGR cuz mediapipe only work on RGB
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        result = pose.process(imgRGB)
+        # print(result.pose_landmarks)
+        if result.pose_landmarks:
+            mpDraw.draw_landmarks(img, result.pose_landmarks, my_pose.POSE_CONNECTIONS)
+
+        # checking video frame rate
+        current_time = time.time()
+        fps = 1 / (current_time - previous_time)
+        previous_time = current_time
+
+        # Writing FrameRate on video
+        cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+
+        #cv2.imshow("Pose detection", img)
+        frame = cv2.imencode('.jpg', img)[1].tobytes()
+        yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        key = cv2.waitKey(20)
+        if key == 27:
+            break
+
 @app.route('/')
 def index():
     return render_template('index.js')
-    
-def gen(camera):
-    while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-               
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen(VideoCamera()),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/video')
+def video():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
