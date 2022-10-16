458c06d4cce897d367793447d995697198598ace
Bob Pham <bobbaopham@gmail.com>
2022-10-16 00:02:20 -0700

    added moving gradient

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import Title from "./components/Title";
 import Cam from './right_angle/templates/index';
 
 function App() {
-  return <div className="bg-gradient-to-tr from-indigo-300 to-purple-400 grid place-items-center h-screen">
+  return <div className="bg-gradient-to-tr from-indigo-300 to-purple-400 animate-gradient-xy grid place-items-center h-screen">
     <Title />
     <Cam />
   </div>;
